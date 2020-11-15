0dd84086db55fe9ccbc69505ca742be53d8958f0
c1184405532 <1184405532@qq.com>
2020-11-15 19:09:41 +0800

    '初始化创建项目'

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import { Button } from 'antd';
+import BaseLayout from 'components/baseLayout';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BaseLayout/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/baseLayout/index.jsx
@@ -0,0 +1,11 @@
+import { Layout } from 'antd';
+import React from 'react';
+const { Header, Footer, Sider, Content } = Layout;
+class baseLayout extends React.Component {
+  render(){
+    return (
+      <div>1111</div>
+    )
+  }
+}
+export default baseLayout;
