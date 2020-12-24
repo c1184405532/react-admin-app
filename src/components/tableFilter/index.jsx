1b405d0ab1873bdae24255e53abff38803e629d0
c1184405532 <1184405532@qq.com>
2020-12-24 21:25:17 +0800

    '添加自定义筛选项'

--- src/components/tableFilter/index.jsx
+++ src/components/tableFilter/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Form, Input, Button, Select, DatePicker } from 'antd';
+import { Form, Input, Button, Select, DatePicker ,Spin } from 'antd';
 import moment from 'moment';
 import './index.less';
 /**
- * columns type Array[object,object,...]
+ * loading 是否处于加载中 当筛选项有下拉列表需要指定默认值，或者其他数据项需要请求后端指定默认值可使用此字段提示用户正在加载。
+ * columns 当前筛选列 type Array[object,object,...]
  * object  = {
  *  type,当前筛选项类型，默认 'input'  可选值有 (select下拉选择，) string
  *  className,当前form.item表单额外类名 string
@@ -11,12 +12,21 @@ import './index.less';
  *  name,点击查询时返回的key字段，如name="age" ，查询时会返回对象集合{age:选择或输入的值} string
  *  rules,表单校验规则，格式同antd form 表单校验一致 array
  *  placeHolder,没有值的情况下占位符 string
- *  initialValue, 当前项默认值 string
+ *  initialValue, 当前项默认值 any
+ *  component: type ReactDom react组件，自定义传入筛选项，注意如果传入antd时间组件，默认值需要设置对应的moment类型，且获取到的也是
+ *  moment类型，需要手动转换一次 value.format("YYYY-MM-DD HH:mm")
+ *  示例 {
+ *         label:'离职日期5',name:'custom',initialValue:moment("2021-11-10 20:30","YYYY-MM-DD HH:mm"),
+ *          component:<DatePicker showTime/>
+ *      } 
+ * 
+ * 
  *  selectOptions Array[object,object,...] 如果type是select状态时，需要传递的下拉菜单数据
- *  object = {
- *   label,展示的文字 string,
- *   value,选中之后提交的实际值 string||number
- *  }
+ *    object = {
+ *      label,展示的文字 string,
+ *      value,选中之后提交的实际值 string||number
+ *    }
+ * 
  * }
  */
 const setInitialValue = (value, type) => {
@@ -25,6 +35,9 @@ const setInitialValue = (value, type) => {
     "select": value ? value : undefined,//select下如果默认值不存在的话，不能赋值为''，否则不显示placeHolder
     "datePicker": value ? moment(value, 'YYYY-MM-DD') : '',
     "datePickerMonth": value ? moment(value, 'YYYY-MM') : '',
+    "datePickerYear": value ? moment(value,'YYYY') : '',
+    "datePickerWeek": value ? moment(value,'YYYY-W') : '',
+    "datePickerQuarter": value ? moment(value,"YYYY-[Q]Q") : '',
   }
   return valueType[type]
 }
@@ -41,7 +54,8 @@ const setFinishValue = (value, type) => {
 }
 class TableFilter extends React.Component {
   static defaultProps = {
-    columns: []
+    columns: [],
+    loading:false,
   }
   state = {
 
@@ -57,11 +71,9 @@ class TableFilter extends React.Component {
       values[obj] = values[obj] ? values[obj] : '';
       columns.forEach(col => {
         //只要类型前10位 === datePicker都会进入此判断 比如datePicker||datePickerMonth
-        if (obj === col['name'] && col.type.slice(0,10) === 'datePicker') {
+        if (obj === col['name'] && col.type && col.type.slice(0,10) === 'datePicker') {
           values[obj] = setFinishValue(values[obj],col.type) 
         }
-        
-        
       })
     }
     console.log(values)
@@ -74,7 +86,7 @@ class TableFilter extends React.Component {
     const fieldsData = columns.map((col) => {
       return {
         name: col.name,
-        value: setInitialValue(col.initialValue, col.type)
+        value: col.type ? setInitialValue(col.initialValue, col.type) : col.initialValue 
       }
     })
     console.log(fieldsData)
@@ -84,7 +96,7 @@ class TableFilter extends React.Component {
     const {
       className = '', label, name, rules = [{ required: false }],
       placeHolder, initialValue = '', type = 'input', selectOptions = [],
-      
+      component
     } = col;
 
     const itemOption = {
@@ -98,8 +110,9 @@ class TableFilter extends React.Component {
     //这里删除的属性都是Form.Item不需要的或者不识别的属性
     delete itemOption.placeHolder
     delete itemOption.selectOptions
+    delete itemOption.component
     let resultCol;
-    if (type === 'input') {
+    if (type === 'input' && !component) {
       resultCol = <Form.Item
         {...itemOption}
       >
@@ -121,7 +134,7 @@ class TableFilter extends React.Component {
       resultCol = <Form.Item
         {...itemOption}
       >
-        <DatePicker  />
+        <DatePicker/>
       </Form.Item>
     }
     if (type === 'datePickerWeek') {
@@ -153,41 +166,38 @@ class TableFilter extends React.Component {
         <DatePicker picker="quarter" />
       </Form.Item>
     }
+    if(component){
+      resultCol = <Form.Item
+        {...itemOption}
+      >
+       {component}
+      </Form.Item>
+    }
     return resultCol
   }
   render() {
-    const { columns } = this.props;
+    const { columns,loading } = this.props;
     return (
       <div className="table-filter-box">
-        <Form
-          ref={(refs) => { this.formRef = refs }}
-          className="form-box"
-          layout="inline"
-          name="basic"
-          initialValues={{ remember: true }}
-          onFinish={this.onFinish}
-          onFinishFailed={() => { }}
-        >
-          {
-            columns.map((col) => this.renderCol(col))
-          }
-
-          {/* <Form.Item
-              label="劳务人员名称"
-              className="row-list"
-              name="username"
-              rules={[{ required: false, message: 'Please input your username!' }]}
-            >
-              <Input placeholder="请输入姓名" />
-            </Form.Item> */}
-
-
-
-        </Form>
-        <div className="btn-box">
-          <Button type="primary" onClick={this.onSearch}>查询</Button>
-          <Button className="reset" onClick={this.onReset}>重置</Button>
-        </div>
+        <Spin spinning={loading}>
+          <Form
+            ref={(refs) => { this.formRef = refs }}
+            className="form-box"
+            layout="inline"
+            name="basic"
+            initialValues={{ remember: true }}
+            onFinish={this.onFinish}
+            onFinishFailed={() => { }}
+          >
+            {
+              columns.map((col) => this.renderCol(col))
+            }
+          </Form>
+          <div className="btn-box">
+            <Button type="primary" onClick={this.onSearch}>查询</Button>
+            <Button className="reset" onClick={this.onReset}>重置</Button>
+          </div>
+        </Spin>
       </div>
     )
   }
--- src/pages/laborPersonnel/index.jsx
+++ src/pages/laborPersonnel/index.jsx
@@ -1,25 +1,52 @@
 import React from 'react';
 import TableFilter from 'components/tableFilter';
+import { DatePicker } from 'antd';
+import moment from 'moment';
 import './index.less';
 
 class LaborPersonnel extends React.Component {
   state = {
-    
+    selectOptions:[],
+    tableFilterLoading:true,
   };
-  
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({
+        selectOptions:[{label:'一组',value:'id1'}]
+      })
+    },2000)
+    setTimeout(()=>{
+      const oldList = this.state.selectOptions.map(item => item)
+      this.setState({
+        selectOptions:[...oldList,{label:'二组',value:'id2'},],
+        tableFilterLoading:false,
+      })
+    },4000)
+    setTimeout(()=>{
+      const oldList = this.state.selectOptions.map(item => item)
+      this.setState({
+        selectOptions:[...oldList,{label:'三组',value:'id3'},]
+      })
+    },6000)
+  };
   render(){
+    const { selectOptions ,tableFilterLoading } = this.state;
     const filterColumns = [
       {label:'姓名',name:'laborName',type:'input'},
       {label:'劳务人员编号',name:'laborCode',type:'input',},
       {
-        label:'劳务人员班组',name:'laborGroups',type:'select',initialValue:'id123456',
-        selectOptions:[{label:'一组',value:'id12345'},{label:'二组',value:'id123456'},]
+        label:'劳务人员班组',name:'laborGroups',type:'select',initialValue:'id2',
+        selectOptions:selectOptions
       },
       {label:'入职日期',name:'entryDate',type:'datePicker',initialValue:'2020-12-11'},
       {label:'离职日期',name:'lizhiDate',type:'datePickerMonth',initialValue:'2021-11'},
-      {label:'离职日期2',name:'lizhiDate2',type:'datePickerWeek',},
-      {label:'离职日期3',name:'lizhiDate3',type:'datePickerYear',},
-      {label:'离职日期4',name:'lizhiDate4',type:'datePickerQuarter',},
+      {label:'离职日期2',name:'lizhiDate2',type:'datePickerWeek',initialValue:'2020-50周'},
+      {label:'离职日期3',name:'lizhiDate3',type:'datePickerYear',initialValue:'2024'},
+      {label:'离职日期4',name:'lizhiDate4',type:'datePickerQuarter',initialValue:'2020-Q3'},
+      {
+        label:'离职日期5',name:'custom',initialValue:moment("2021-11-10 20:30","YYYY-MM-DD HH:mm"),
+        component:<DatePicker showTime/>
+      },
       //{label:'劳务人员编号',name:'laborCodes',type:'input',placeHolder:'请输入'},
       //{label:'劳务人员编号',name:'laborCodess',type:'input',placeHolder:'请输入'},
       //{label:'劳务人员编号',name:'laborCodesss',type:'input',placeHolder:'请输入'},
@@ -28,6 +55,7 @@ class LaborPersonnel extends React.Component {
       <div className="labor-list-box">
         <TableFilter
           columns={filterColumns}
+          loading={tableFilterLoading}
         />
       </div>
     )
